6e6cdef84104cbe840c2618f0961da7ececae7e1
Troy Blank <troy@troyblank.com>
2016-06-05 12:26:51 -0500

    made private key optional

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,11 +2,14 @@
 
 module.exports = function (grunt) {
     const credentialsPath = 'credentials/config.json',
-        credentials = grunt.file.exists(credentialsPath) ? grunt.file.readJSON(credentialsPath) : null;
+        privateKeyPath = 'credentials/id_rsa',
+        credentials = grunt.file.exists(credentialsPath) ? grunt.file.readJSON(credentialsPath) : null,
+        privateKey = grunt.file.exists(privateKeyPath) ? grunt.file.read(privateKeyPath) : null;
 
     require('load-grunt-config')(grunt, {
         data: {
-            credentials: credentials
+            credentials: credentials,
+            privateKey: privateKey
         }
     });
 };

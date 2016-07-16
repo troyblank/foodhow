9b265f4b06d777111328661f59efe9884b27e506
Troy Blank <troy@troyblank.com>
2016-07-16 11:15:28 -0500

    insured no results only shows with 0 ingredients

--- assets/js/components/shoppingList.js
+++ assets/js/components/shoppingList.js
@@ -4,20 +4,21 @@ import {
     NoResultMessage }
     from '@troyblank/food-how-components';
 
-let ingredientNames;
+let ingredientNames, showNoResult;
 
 export default class ShoppingList extends Component {
     constructor(props) {
         super(props);
 
         const ingredients = props.store.getState().ingredients;
+        showNoResult = 0 === ingredients.length;
         ingredientNames = ingredients.map(obj => obj.name);
     }
 
     render() {
         return (
           <section>
-            <NoResultMessage headline={'This list is empty'} message={'There is no ingredients on your list, add some to make a shopping list.'} />
+            {showNoResult && <NoResultMessage headline={'This list is empty'} message={'There is no ingredients on your list, add some to make a shopping list.'} />}
             <GetShoppingList list={ingredientNames} />
           </section>
         );

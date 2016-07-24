88247755fec8b45cd8d7031b3da8762a01349737
Troy Blank <troy@troyblank.com>
2016-07-23 19:55:41 -0500

    added ingredient handler

--- assets/js/components/shoppingList.js
+++ assets/js/components/shoppingList.js
@@ -1,28 +1,34 @@
 import React, { Component } from 'react';
+import { toggleIngredient } from '../actions/index';
 import {
     GetShoppingList,
     GotShoppingList,
     NoResultMessage }
     from '@troyblank/food-how-components';
 
-let showNoResult, getIngredients, gotIngredients;
+let store, showNoResult, getIngredients, gotIngredients;
 
 export default class ShoppingList extends Component {
     constructor(props) {
         super(props);
 
         const ingredients = props.store.getState().ingredients;
+        store = props.store;
         getIngredients = ingredients.filter((ingredient) => !ingredient.checked);
         gotIngredients = ingredients.filter((ingredient) => ingredient.checked);
         showNoResult = 0 === ingredients.length;
+    }
+
+    ingredientClickHand(id) {
+        store.dispatch(toggleIngredient(id));
     }
 
     render() {
         return (
           <section>
             {showNoResult && <NoResultMessage headline={'This list is empty'} message={'There is no ingredients on your list, add some to make a shopping list.'} />}
-            <GetShoppingList list={getIngredients} />
-            <GotShoppingList list={gotIngredients} />
+            <GetShoppingList list={getIngredients} ingredientClickHand={this.ingredientClickHand} />
+            <GotShoppingList list={gotIngredients} ingredientClickHand={this.ingredientClickHand} />
           </section>
         );
     }

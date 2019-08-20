90e884aa86e0be76e2d1170b4f59771e6a8b5659
Troy Blank <troy.blank@topbox.io>
2019-08-19 23:15:19 -0500

    tied shopping list to localStorage

--- src/store.js
+++ src/store.js
@@ -2,8 +2,22 @@ import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 
+const foodHowShoppingList = 'FOOD_HOW_SHOPPING_LIST';
+
 const enhancers = compose(
     applyMiddleware(thunk)
 );
 
-export default createStore(reducers, {}, enhancers);
+const shoppingListLocalStore = typeof localStorage !== 'undefined' ? JSON.parse(localStorage.getItem(foodHowShoppingList)) : [];
+const defaultState = { shoppingList: { shoppingList: shoppingListLocalStore } };
+
+const store = createStore(reducers, defaultState, enhancers);
+
+store.subscribe(() => {
+    const { shoppingList: shoppingListStore } = store.getState();
+    const { shoppingList } = shoppingListStore;
+
+    localStorage.setItem(foodHowShoppingList, JSON.stringify(shoppingList));
+});
+
+export default store;

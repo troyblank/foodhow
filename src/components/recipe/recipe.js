8993cd2a7c7d680460bf58cda25d4f90e26426ec
Troy Blank <troy@troyblank.com>
2020-02-13 19:15:14 -0700

    fixed html display for meta

--- src/components/recipe/recipe.js
+++ src/components/recipe/recipe.js
@@ -48,7 +48,7 @@ export default class Recipe extends PureComponent {
           <section className={'recipe'}>
             <header>
               <h1>{ title }</h1>
-              { true && <h3>{meta}</h3> }
+              { meta && <h3 dangerouslySetInnerHTML={{ __html: dompurify.sanitize(meta) }} /> }
             </header>
             <section>
               <h2>Ingredients</h2>

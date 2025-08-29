ac54a0c00cd1eaa6c03e511566064c9d77dc6d29
Troy Blank <troy@troyblank.com>
2025-08-29 17:16:57 -0500

    Moved static files over to public due to upgrade next.js changes.

--- src/components/recipe/recipe.tsx
+++ src/components/recipe/recipe.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, type FunctionComponent } from 'react';
 import dompurify from 'dompurify';
 import { isArray } from 'lodash';
-import { Directions, IngredientList } from '../'
+import { Directions, IngredientList } from '..';
 
 export function objectifyIngredients(polymorphicIngredients) {
     // ingredients can be an array or object:
@@ -23,40 +23,40 @@ export const Recipe: FunctionComponent<RecipeProps> = ({ fileName }) => {
     const [recipe, setRecipe] = useState(null);
 
     useEffect(() => {
-        fetch(`/static/recipes/${fileName.toLowerCase()}.json`)
+        fetch(`/recipes/${fileName.toLowerCase()}.json`)
             .then((response) => response.json())
             .then((recipe) => {
-                setRecipe(recipe)
+                setRecipe(recipe);
             });
-    }, [])
+    }, []);
 
     if (!recipe) return null;
 
     const { title, meta, ingredients: polymorphicIngredients, directions } = recipe;
     const ingredients = objectifyIngredients(polymorphicIngredients);
-    const ingredientTitles = Object.keys(ingredients)
+    const ingredientTitles = Object.keys(ingredients);
 
     return (
-        <section className={'recipe'}>
-            <header>
-               <h1>{ title }</h1>
-               { meta && <h3 dangerouslySetInnerHTML={{ __html: dompurify.sanitize(meta) }} /> }
-            </header>
-            <section>
-               <h2>Ingredients</h2>
-               { ingredientTitles.map((title) => (
-                    <IngredientList
-                        title={title}
-                        ingredients={ingredients[title]}
-                        fileName={fileName}
-                        key={title}
-                    />
+      <section className={'recipe'}>
+        <header>
+          <h1>{ title }</h1>
+          { meta && <h3 dangerouslySetInnerHTML={{ __html: dompurify.sanitize(meta) }} /> }
+        </header>
+        <section>
+          <h2>Ingredients</h2>
+          { ingredientTitles.map((title) => (
+            <IngredientList
+              title={title}
+              ingredients={ingredients[title]}
+              fileName={fileName}
+              key={title}
+            />
                ))}
-             </section>
-             <section>
-                <h2>Directions</h2>
-                <Directions steps={directions} />
-             </section>
         </section>
-    )
-}
+        <section>
+          <h2>Directions</h2>
+          <Directions steps={directions} />
+        </section>
+      </section>
+    );
+};

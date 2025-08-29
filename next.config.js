e1515d662750abbdbe5a12c6f6f6c9615e2f5ff7
Troy Blank <troy@troyblank.com>
2025-08-29 16:56:30 -0500

    Fixes 404s due to next static export

--- next.config.js
+++ next.config.js
@@ -2,10 +2,5 @@ module.exports = {
     eslint: {
         ignoreDuringBuilds: true
     },
-    exportPathMap: () => ({
-        '/': { page: '/' },
-        '/guide': { page: '/guide' },
-        '/shoppingList': { page: '/shoppingList' }
-    }),
     reactStrictMode: true
 };
